b5e63c8404c51e841b60c7ad2f784be65e4f4247
yam1c6a <yam1c6a@gmail.com>
2025-12-19 16:48:13 +0900

    v0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 34
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
