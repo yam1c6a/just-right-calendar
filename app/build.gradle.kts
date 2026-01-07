8986feda891be2e0c88808e3d2d768c5daba4c96
yam1c6a <yam1c6a@gmail.com>
2026-01-07 10:45:20 +0900

    Release v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 34
         targetSdk = 36
 
-        versionCode = 1
-        versionName = "0.9.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
